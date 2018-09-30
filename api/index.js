3baa494e086ceacbf7c8a1f72d98ae8e46205960
hecmonn <hecmonn@gmail.com>
2018-09-30 10:48:40 -0500

    associating tickets w users

--- api/index.js
+++ api/index.js
@@ -21,18 +21,21 @@ const query=(sql,res)=>{
 
 router.post('/register',(req,res)=>{
     const {source,payload}=req.body;
+    let sourceId=payload.id;
     const sql=`select id,source from users where email='${payload.email}' and source=${source}`;
     con.query(sql,(err,response,fields)=>{
+        if(err) console.error('Error registering user: ',err);
         if(isEmpty(response)){
             let sql=`insert into users(source,email,name) values(1,'${payload.email}','${payload.name}')`;
             con.query(sql,(err,response,fields)=>{
                 let payload;
+                let userId=response.insertId;
                 if(err) payload={msg:'Error registering user',success:false,errorDesc:err}
-                else payload={msg:'User registered succesfully',success:true};
+                else payload={msg:'User registered succesfully',success:true,userId,source,sourceId};
                 res.json(payload);
             });
         } else {
-            res.json({msg:'Welcome back',success:true})
+            res.json({msg:'Welcome back',success:true,userId:response[0].id,sourceId,source})
         }
     });
 });
@@ -78,27 +81,28 @@ router.post('/save-ticket',(req,res)=>{
     let tableProviderId=table.id;
     let rcProviderId=revenueCenter.id;
     let sqlRc=`select id from revenue_centers where location_id=${locationId} and rc_provider_id='${rcProviderId}'`;
-    console.log('sqlRc: ',sqlRc);
     con.query(sqlRc,(err,response,fields)=>{
         if(!isEmpty(response)){
             if(err) console.error('Error getting rc: ',err);
             let rcId=response[0].id;
             let sqlTable=`select id from tables where location_id=${locationId} and table_provider_id='${tableProviderId}'`;
+            console.log('sql table: ',sqlTable);
             con.query(sqlTable,(err,response,fields)=>{
                 if(!isEmpty(response)){
                     if(err) console.error('Error getting table: ',err);
                     let tableId=response[0].id;
                     let openFix=open?1:0;
                     let sqlSt=`insert into tickets(table_id,revenue_center_id,name,open,opened_at,location_id,provider_id,ticket_provider_id) values(${tableId},${rcId},'${name}','${openFix}',from_unixtime('${openedAt}'),${locationId},${providerId},'${ticketId}')`;
-                    console.log('sqlSt: ',sqlSt);
                     con.query(sqlSt,(err,response,fields)=>{
                         if(err) console.error('Error saving ticket: ',err);
-                        console.log('tikcet insert id: ',response.insertId);
                         let ticketPhoodId=response.insertId;
-                        let sqlSu=`insert into users_tickets (user_id,ticket_id,is_admin) values(1,${ticketPhoodId},1)`;
+                        //to update
+                        let sqlSu=`insert into users_tickets (user_id,ticket_id,is_admin) values(${user.id},${ticketPhoodId},${user.admin})`;
+                        console.log('sql Su: ',sqlSu);
                         con.query(sqlSu,(err,response,fields)=>{
                             if(err) console.error('Error saving user: ',err);
-                            res.json({ok:true,msg:'Ticket opened succesfully'});
+                            console.log('ticket phood id: ',ticketPhoodId);
+                            res.json({ok:true,ticketPhoodId,msg:'Ticket opened succesfully'});
                         });
                     });
                 } else {
@@ -109,6 +113,16 @@ router.post('/save-ticket',(req,res)=>{
             console.log('revenue center doesnt exist');
             res.json({ok:false,msg:'Revenue center not available'});
         }
+    });
+});
+
+router.post('/get-ticket-admin',(req,res)=>{
+    const {ticketId}=req.body;
+    let sql=`select user_id from users_tickets where is_admin=1 and ticket_id=${ticketId}`;
+    con.query(sql,(err,response,fields)=>{
+        if(err) console.error('Error selecting ticket admin: ',err);
+        console.log('ticket admin sql: ',sql);
+        res.json({response});
     });
 });
 
